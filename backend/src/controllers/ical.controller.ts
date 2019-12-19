4ce330cbc74a80467fbe0c8b5c2c9d3eadde2a85
Patrick Mettraux <patrick@mettraux-web.net>
2019-12-19 09:23:13 +0100

    Backend (#3)
    * backend structure
    
    * parsing and formatting of start and end date
    
    * arser completed
    
    * fix docker
    
    * ical generated
    
    * use variable from query

---
+++ backend/src/controllers/ical.controller.ts
@@ -0,0 +1,45 @@
+import * as express from 'express'
+import { Request, Response } from 'express'
+import IControllerBase from '../interfaces/IControllerBase.interface'
+import { get } from 'request-promise';
+import { jsonParser, IJsonScheduleGlobal } from '../lib/HesScheduleParser';
+import * as ical from 'ical-generator';
+import { json } from 'body-parser';
+
+const basePlanningUrl: string = 'http://mobileapps.hevs.ch/HoraireBellevue/Planning.aspx?NoClasse=';
+class HomeController implements IControllerBase {
+  public router: express.Router = express.Router();
+
+  constructor() {
+    this.initRoutes()
+  }
+
+  public initRoutes() {
+    this.router.get('/v1/ical', this.index)
+  }
+
+  index = async (req: Request, res: Response) => {
+    const classNo: number = req.query.classNo;
+
+    const htmlContent = await get(`${basePlanningUrl}${classNo}`);
+
+    const sortedData: IJsonScheduleGlobal = jsonParser(htmlContent);
+    const cal = ical({domain: 'hevs.ch', name: `${sortedData.cat} - ${sortedData.classNo}`});
+    
+    sortedData.weeks.forEach(week => {
+      week.lessons.forEach(lesson => {
+        cal.createEvent({
+          start: lesson.start,
+          end: lesson.end,
+          summary: lesson.course,
+          description: `Teacher: ${lesson.teacher}`,
+          location: lesson.location,
+        });
+      })
+    })
+
+    cal.serve(res);
+  }
+}
+
+export default HomeController
