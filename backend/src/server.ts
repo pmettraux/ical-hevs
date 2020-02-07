5b643ec9f5a2332dc9f491190df4d713bea9065f
Patrick Mettraux <patrick@mettraux-web.net>
2020-02-08 00:43:28 +0100

    progress

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,7 +6,7 @@ import loggerMiddleware from './middlewares/logger'
 import IcalController from './controllers/ical.controller'
 
 const app = new App({
-  port: 80,
+  port: parseInt(process.env.PORT),
   controllers: [
     new IcalController(),
   ],
